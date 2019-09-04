07885918187955ccd8af692c5231270905b9b512
Drew Hill <l.drew.hill@gmail.com>
2019-09-04 07:17:40 -0700

    -sql fetcher updated

--- 03_sqlstudio/authorize_cloudshell.sh
+++ 03_sqlstudio/authorize_cloudshell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud sql instances patch flights \
+gcloud sql instances patch flights1 \
     --authorized-networks `wget -qO - http://ipecho.net/plain`/32
--- 03_sqlstudio/populate_table.sh
+++ 03_sqlstudio/populate_table.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 BUCKET=$1
-echo "Populating Cloud SQL instance flights1 from gs://${BUCKET}/flights1/raw/..."
+echo "Populating Cloud SQL instance flights from gs://${BUCKET}/flights/raw/..."
 
 # To run mysqlimport and mysql, authorize CloudShell
 bash authorize_cloudshell.sh
@@ -16,12 +16,11 @@ counter=0
 #for FILE in $(gsutil ls gs://${BUCKET}/flights1/raw/2015*.csv); do
 #   gsutil cp $FILE flights1.csv-${counter}
 for FILE in 201501.csv 201507.csv; do
-   gsutil cp gs://${BUCKET}/flights1/raw/$FILE flights1.csv-${counter}
+   gsutil cp gs://dsci_prac_test_dh_1/flights/raw/$FILE flights1.csv-${counter}
    counter=$((counter+1))
 done
 
 # import csv files
-MYSQLIP=$(gcloud sql instances describe flights1 --format="value(ipAddresses.ipAddress)")
+MYSQLIP=$(gcloud sql instances describe flights --format="value(ipAddresses.ipAddress)")
 mysqlimport --local --host=$MYSQLIP --user=root --ignore-lines=1 --fields-terminated-by=',' --password bts flights1.csv-*
-rm flights1.csv-* 
-
+rm flights.csv-* 
